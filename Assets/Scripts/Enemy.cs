a90ad059764062ab71690fd0016dbebae73162f7
juanvalag2019 <juanvalag@unisabana.edu.co>
2021-08-29 17:20:40 -0500

    se añadio corazones a los animales

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,11 +6,16 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] float speed;
     [SerializeField] bool direction;
+    [SerializeField] Sprite[] heartSkins;
     private float maxX, minX;
+    private Transform[] hearts;
+    private int lives;
 
     // Start is called before the first frame update
     void Start()
     {
+        hearts = gameObject.GetComponentsInChildren<Transform>();
+        lives = hearts.Length - 1;
         float tamX = (GetComponent<SpriteRenderer>()).bounds.size.x;
         float tamY = (GetComponent<SpriteRenderer>()).bounds.size.y;
         Vector2 esquinaInfDer = Camera.main.ViewportToWorldPoint(new Vector2(1, 0));
@@ -41,7 +46,16 @@ public class Enemy : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Bullet"))
         {
-            Destroy(this.gameObject);
+            if (lives > 1)
+            {
+                SpriteRenderer spriteRenderer = hearts[lives].gameObject.GetComponent<SpriteRenderer>();
+                spriteRenderer.sprite = heartSkins[1];
+                lives--;
+            }
+            else
+            {
+                Destroy(this.gameObject);
+            }
         }
     }
 }
